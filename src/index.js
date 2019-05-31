8c1f18b8ca62baa866a967291c11a963b6004dde
trfrancisianaquino <tr.francisian.aquino@gmail.com>
2019-06-01 00:39:40 +0800

    :sparkles: Add score function

--- src/index.js
+++ src/index.js
@@ -18,50 +18,48 @@ function ToggleLetter(props) {
     );
 }
 
-// function checkSOS(row, column, squares) {
-//     const lines = [
-//         [row, column, row, column+1, row, column+2],
-//         [row, column, row+1, column, row+2, column],
-//         [row, column, row+1, column+1, row+2, column+2],
-//         [row, column, row-1, column+1, row-2, column+2],
-
-//         [row, column-1, row, column, row, column+1],
-//         [row-1, column, row, column, row+1, column],
-//         [row-1, column-1, row, column, row+1, column+1],
-//         [row-1, column+1, row, column, row+1, column-1],
-
-//         [row, column-2, row, column-1, row, column],
-//         [row-2, column, row-1, column, row, column],
-//         [row-2, column-2, row-1, column-1, row, column],
-//         [row+2, column-2, row+1, column-1, row, column],
-//     ];
-
-//     for (let i = 0; i < lines.length; i++) {
-//         const [ar, ac, br, bc, cr, cc] = lines[i];
-
-//         console.log('ar:' + ar + ' ac:' + ac + ' br:' + br + ' bc:' + bc + ' cr:' + cr + ' cc:' + cc);
-
-//         // if (squares[ar][ac] === 'S' && squares[br][bc] === 'O' && squares[cr][cc] === 'S') {
-//         //     console.log('Scores');
-//         // }
-
-//         const pattern = squares[ar][ac] + squares[br][bc] + squares[cr][cc];
-
-//         if (pattern === 'SOS') {
-//             console.log('You Scored');
-//         }
-//     }
-//     return null;
-
-// }
-
 class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
             squares: Array(18).fill(null).map(x => Array(18).fill(null)),
-            xIsNext: true
+            xIsNext: true,
+            isPlayerOne: true,
+            playerOneScore: 0,
+            playerTwoScore: 0
         };
+    }
+
+    renderSquare(i, j) {
+        return (
+            <Square
+                value={this.state.squares[i][j]}
+                onClick={() => this.handleSquareClick(i, j)}
+            />
+        );
+    }
+
+    handleSquareClick(i, j) {
+        const squares = this.state.squares.slice();
+        if (squares[i][j] == null) {
+            squares[i][j] = this.state.xIsNext ? 'S' : 'O';
+            this.setState({
+                squares: squares,
+            });
+        }
+        this.checkSOS(i, j)
+    }
+
+    renderToggleLetter() {
+        return (
+            <ToggleLetter onClick={() => this.handleButtonClick()} />
+        );
+    }
+
+    handleButtonClick() {
+        this.setState({
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     checkSOS(row, column) {
@@ -85,53 +83,43 @@ class Board extends Component {
             const [ar, ac, br, bc, cr, cc] = lines[i];
 
             if ((ar >= 0 && ar <= 17) && (br >= 0 && br <= 17) && (cr >= 0 && cr <= 17)) {
-                // console.log('ar:' + ar + ' ac:' + ac + ' br:' + br + ' bc:' + bc + ' cr:' + cr + ' cc:' + cc);
 
                 const pattern = this.state.squares[ar][ac] + this.state.squares[br][bc] + this.state.squares[cr][cc];
 
                 if (pattern === 'SOS') {
-                    console.log('You Scored');
+                    this.scores();
                 }
             }
         }
     }
 
-    handleSquareClick(i, j) {
-        const squares = this.state.squares.slice();
-        if (squares[i][j] == null) {
-            squares[i][j] = this.state.xIsNext ? 'S' : 'O';
+    scores() {
+        let currentScore = this.state.isPlayerOne ? this.state.playerOneScore : this.state.playerTwoScore;
+        const player = this.state.isPlayerOne ? 'one' : 'two';
+        currentScore += 1;
+
+        if (this.state.isPlayerOne) {
             this.setState({
-                squares: squares,
-            });
+                playerOneScore: currentScore
+            })
         }
-        this.checkSOS(i, j)
-    }
+        else {
+            this.setState({
+                playerTwoScore: currentScore
+            })
+        }
 
-    renderSquare(i, j) {
-        return (
-            <Square
-                value={this.state.squares[i][j]}
-                onClick={() => this.handleSquareClick(i, j)}
-            />
-        );
-    }
+        console.log(player + ": " + currentScore);
 
-    handleButtonClick() {
         this.setState({
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
-    renderToggleLetter() {
-        return (
-            <ToggleLetter onClick={() => this.handleButtonClick()} />
-        );
+            isPlayerOne: !this.state.isPlayerOne
+        })
     }
 
     render() {
         const status = 'Next player: ' + (this.state.xIsNext ? 'S' : 'O');
 
-        const grid = this.state.squares
+        const grid = this.state.squares;
 
         const board = grid.map((row, i) => {
             return (
@@ -153,6 +141,14 @@ class Board extends Component {
 }
 
 class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // playerOneScore: 0,
+            // playerTwoScore: 0
+        };
+    }
+
     render() {
         return (
             <div className="game">
